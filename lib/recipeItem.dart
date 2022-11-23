95d6bcf4ddfb78592702cd0b7da321178f21c603
JF-PS <jfps.dev21@gmail.com>
2022-11-23 10:34:23 +0100

    feature(list): navigation

--- lib/recipeItem.dart
+++ lib/recipeItem.dart
@@ -1,41 +1,53 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:test_test/recipe.dart';
+import 'package:test_test/recipeScreen.dart';
+import 'package:test_test/reicipeListScreen.dart';
 
 class RecipeItem extends StatelessWidget {
   final Recipe recipe;
-  const RecipeItem({Key key = const Key("any_key"), required this.recipe}): super(key: key);
+  const RecipeItem({Key key = const Key("any_key"), required this.recipe})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(8),
-      elevation: 8,
-      child: Row(
-        children: [
-          CachedNetworkImage(
-            imageUrl: recipe.imageUrl,
-            placeholder: (context, url) =>
-                Center(child: CircularProgressIndicator()),
-            errorWidget: (context, url, error) => Icon(Icons.error),
-            width: 100,
-            height: 100,
-            fit: BoxFit.cover,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(recipe.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                ),
-                Text(recipe.user, style: TextStyle(color: Colors.grey[500], fontSize: 16))
-              ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => RecipeScreen(recipe: recipe,)));
+      },
+      child: Card(
+        margin: const EdgeInsets.all(8),
+        elevation: 8,
+        child: Row(
+          children: [
+            CachedNetworkImage(
+              imageUrl: recipe.imageUrl,
+              placeholder: (context, url) =>
+                  Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+              width: 100,
+              height: 100,
+              fit: BoxFit.cover,
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(recipe.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20)),
+                  ),
+                  Text(recipe.user,
+                      style: TextStyle(color: Colors.grey[500], fontSize: 16))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
